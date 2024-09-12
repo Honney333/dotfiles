c985c482aa56af6cfa2e355f47b4335e4f35fa7f
Honney333 <h.gehrmann@proton.me>
2024-09-12 19:36:10 +0000

    waybar moduels

---
+++ .config/waybar/modules/cpu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Read the CPU temperature from hwmon (replace hwmon2/temp1_input with your actual path)
+cpu_temp=$(cat /sys/class/hwmon/hwmon5/temp1_input)
+
+# Convert millidegrees Celsius to degrees Celsius
+cpu_temp=$(echo "scale=1; $cpu_temp / 1000" | bc)
+
+cpu_usage=$(top -bn1 | awk '/Cpu/ { print $2}')
+
+# Output the CPU temperature
+echo "CPU: ${cpu_temp}°C | ${cpu_usage}%"
---
+++ .config/waybar/modules/gpu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Read the GPU temperature from hwmon (replace hwmon4/temp1_input with your actual path)
+gpu_temp=$(cat /sys/class/hwmon/hwmon3/temp2_input)
+
+# Convert millidegrees Celsius to degrees Celsius
+gpu_temp=$(echo "scale=1; $gpu_temp / 1000" | bc)
+
+gpu_usage=$(rocm-smi --showuse | grep 'GPU\[0\]' | awk '{print $6}')
+
+
+# Output the GPU temperature
+echo "GPU: ${gpu_temp}°C | ${gpu_usage}%"
