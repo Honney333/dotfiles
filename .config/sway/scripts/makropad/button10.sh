2bd29f83c816366deb9db61cf9080ae5d34b9383
Honney333 <h.gehrmann@proton.me>
2024-09-21 22:46:08 +0000

    updates on sway config

---
+++ .config/sway/scripts/makropad/button10.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Add shared script Functions
+source "$HOME/.config/function_helper.sh"
+
+send_notification "low" "Pressed Makropad Button" "Row 3, Colums 2" "$HOME/.icons/BeautyLine-Garuda/apps/scalable/keyboard.svg"
