993da4e17cb9a1cae06eea6432a4505d43eb3169
Honney333 <h.gehrmann@proton.me>
2024-10-02 20:53:27 +0000

    update

--- .config/waybar/modules/weather.sh
+++ .config/waybar/modules/weather.sh
@@ -3,6 +3,11 @@
 cachedir=~/.cache/rbn
 cachefile=${0##*/}-$1
 
+# Check if cache file has more than 6 lines and delete if true
+if [ -f $cachedir/$cachefile ] && [ $(wc -l < $cachedir/$cachefile) -gt 6 ]; then
+    rm -f $cachedir/$cachefile
+fi
+
 if [ ! -d $cachedir ]; then
     mkdir -p $cachedir
 fi
@@ -18,23 +23,27 @@ IFS=$'\n'
 
 cacheage=$(($(date +%s) - $(stat -c '%Y' "$cachedir/$cachefile")))
 if [ $cacheage -gt 1720 ] || [ ! -s $cachedir/$cachefile ]; then
-    data=($(curl -s https://en.wttr.in/$1\?0qnT 2>&1))
+    data=($(curl -s https://en.wttr.in/Dallgow-Döberitz\?0qnT 2>&1))
     echo ${data[0]} | cut -f1 -d, > $cachedir/$cachefile
     echo ${data[1]} | sed -E 's/^.{15}//' >> $cachedir/$cachefile
     echo ${data[2]} | sed -E 's/^.{15}//' >> $cachedir/$cachefile
+    echo ${data[3]} | sed -E 's/^.{15}//' >> $cachedir/$cachefile
+    echo ${data[4]} | sed -E 's/^.{15}//' >> $cachedir/$cachefile
+    echo ${data[5]} | sed -E 's/^.{15}//' >> $cachedir/$cachefile
 fi
 
 weather=($(cat $cachedir/$cachefile))
-
-# Debugging: Output the entire weather array
-# echo "Weather array: ${weather[@]}"
 
 # Restore IFS
 IFS=$SAVEIFS
 
 # Split temperature and condition from weather[2]
-temperature=$(echo ${weather[2]} | awk '{print $1 $2}')
+city=$(echo ${weather[0]})
 condition=$(echo ${weather[1]})
+temperature=$(echo ${weather[2]} | awk '{print $1 $2}')
+wind-speed=$(echo ${weather[2]} | awk '{print $1 $2}')
+visibility=$(echo ${weather[3]} | awk '{print $1 $2}')
+precipitation=$(echo ${weather[4]} | awk '{print $1 $2}')
 
 # Debugging: Output temperature and condition
 # echo "Temperature: $temperature"
@@ -76,10 +85,19 @@ case $(echo ${weather[1]##*,} | tr '[:upper:]' '[:lower:]') in
     condition_icon=" "
     ;;
 *)
-    condition_icon=" "  # Default if no condition matches
+    condition_icon=" "  # Default if no condition matches
     ;;
 esac
 
 #echo $temp $condition
 
-echo -e "{\"text\":\""$temperature $condition_icon $condition"\", \"alt\":\""${weather[0]}"\", \"tooltip\":\""${weather[0]}: $temperature $condition"\"}"
+# Generate the final output string
+output=$(echo -e "{\"text\":\""$temperature $condition_icon $condition"\", \"alt\":\""${weather[0]}"\", \"tooltip\":\""$city:  : ${wind-speed}  : $visibility : $precipitation"\"}")
+
+# Check for invalid UTF-8 characters in the output
+if ! echo "$output" | iconv -f UTF-8 -t UTF-8 >/dev/null 2>&1; then
+    echo "{\"text\":\""Error"\"}"
+    rm -f $cachedir/$cachefile
+else
+    echo "$output"
+fi
